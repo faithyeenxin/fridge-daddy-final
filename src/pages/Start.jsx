b24176eec275e9f89069fe12e05def73b55d00ee
Faith Ye <faith.ye@hotmail.com>
2022-08-25 09:49:51 +0800

    bug fixed

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HeroSection from "../components/display/HeroSection";
 import Instructions from "../components/display/Instructions";
 import HeroSection from "../components/display/HeroSection";
 const Start = () => {
