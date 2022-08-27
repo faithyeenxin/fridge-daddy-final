d15390506a5fbe1eb535cda5646bbcca0b6d386f
Faith Ye <faith.ye@hotmail.com>
2022-08-27 14:33:02 +0800

    cleaned up code for useReducer and useContext

--- src/components/list/MyListItem.jsx
+++ src/components/list/MyListItem.jsx
@@ -1,27 +1,18 @@
-import React, { useContext } from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Avatar from "@mui/material/Avatar";
 import RestoreFromTrashIcon from "@mui/icons-material/RestoreFromTrash";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import FolderIcon from "@mui/icons-material/Folder";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Card, CardActions, CardHeader, CardContent } from "@mui/material";
-import { Container } from "@mui/system";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 
-// import mockUserData from "../testingFolder/mockUserData";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import DataContext from "../../contextStore/data-context";
 
 const buttonSx = {
--- src/contextStore/DataProvider.jsx
+++ src/contextStore/DataProvider.jsx
@@ -1,63 +1,45 @@
-import React, { useState, useReducer } from "react";
+import React, { useReducer } from "react";
 import DataContext from "./data-context.js";
 
-// 4) a constant that stores the default state
 const defaultDataState = { evergreen: [], rotten: [], trashed: [] };
 
-// 1) the action will be dispatched by us later in the function
-// 2) the state is the latest snapshot of the state managed by the reducer
-// 3) the reducer will then return a new state snapshot
 const dataReducer = (state, action) => {
-  // 13) if statement will check which type of action we are dispatching
-  if (action.type === "ADD_EVERGREEN") {
-    // 14) .concat() doesn't edit the existing array but returns a new array
-    // 15) this is important because we need to update our state in an immutable way
-    const updatedEvergreen = state.evergreen.concat(action.item);
-    const updatedRotten = state.rotten;
-    const updatedTrashed = state.trashed;
+  switch (action.type) {
+    case "ADD_EVERGREEN":
+      const updatedEvergreen = state.evergreen.concat(action.item);
+      const updatedRotten = state.rotten;
+      const updatedTrashed = state.trashed;
 
-    // 16) we will then return our new state
-    return {
-      evergreen: updatedEvergreen,
-      rotten: updatedRotten,
-      trashed: updatedTrashed,
-    };
+      return {
+        evergreen: updatedEvergreen,
+        rotten: updatedRotten,
+        trashed: updatedTrashed,
+      };
+      break;
+    default:
+      return defaultDataState;
+      break;
   }
-  return defaultDataState;
 };
 
 const DataProvider = (props) => {
-  // 5) we will then call the reducer inside our provider
-  // 6) the first argument is the reducer function we created outside
-  // 7) the second argument is the initial state which we set as the default state
-  // 8) useReducer will then return an array with exactly 2 elements- which we can destructure
-  // 9) the first element is our state snapshot
-  // 10) the second element is a function that allows us to dispatch an action to the reducer
   const [dataState, dispatchDataAction] = useReducer(
     dataReducer,
     defaultDataState
   );
 
-  // 11) the dispatch function will take in an object that have:
-  // --> a property that helps us identify what action it will dispatch
-  //  [this allows us to run different code in the reducer function (dataReducer)]
-  // --> another property will pass the item that will be part of action
-  // 12) we will now go to the reducer function to add some logic (see reducer function- dataReducer)
   const addEvergreenHandler = (item) => {
     dispatchDataAction({ type: "ADD_EVERGREEN", item: item });
   };
-  // const removeEvergreenHandler = (item) => {
-  //   dispatchDataAction({ type: "REMOVE_EVERGREEN", item: item });
-  // };
 
+  // const removeEvergreenHandler = (item) => {};
   // const removeFromTrashHandler = (item) => {};
   // const checkEvergreenHandler = (item) => {};
   // const tranferEvergreenToRottenHandler = (item) => {};
-
   // const transferEvergreenToTrashedHandler = (item) => {};
 
   const dataContext = {
-    //this is the concrete data which we will update overtime,
+    //this is the concrete data which we will update overtime and pass to our context
     evergreen: dataState.evergreen,
     rotten: dataState.rotten,
     trashed: dataState.trashed,
